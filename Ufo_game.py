b508a20e5c89a3320417271f1ef38841407fedf9
xMidved <mkosovseeds@gmail.com>
2026-02-20 11:56:56 -0500

    Made it go from sides instead of top and made them spawn by themselves every 2 seconds

--- Ufo_game.py
+++ Ufo_game.py
@@ -1,54 +1,60 @@
 import pygame
 import random
 
+
 pygame.init()
-screen = pygame.display.set_mode((600, 400))
+screen = pygame.display.set_mode((600, 800))
 clock = pygame.time.Clock()
 
-balls = []
-gravity = 0.3
 
-SPAWN_DELAY = 2000  
+balls = []
+gravity = 0.5
 spawn_timer = 0
 
-class Ball:
-    def __init__(self):
-        self.radius = 15
-        self.x = random.randint(self.radius, 600 - self.radius)
-        self.y = 0
-        self.vy = 0
-
-    def update(self):
-        self.vy += gravity
-        self.y += self.vy
-
-        # bounce on floor
-        if self.y + self.radius > 400:
-            self.y = 400 - self.radius
-            self.vy *= -0.8
-
-    def draw(self):
-        pygame.draw.circle(screen, (0, 200, 255), (int(self.x), int(self.y)), self.radius)
 
 running = True
 while running:
-    dt = clock.tick(60)
-    spawn_timer += dt
-    screen.fill((30, 30, 30))
+   fps = clock.tick(60)
+   spawn_timer += fps
+   screen.fill((30, 30, 30))
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
 
-    # spawn ball every few seconds
-    if spawn_timer > SPAWN_DELAY:
-        balls.append(Ball())
-        spawn_timer = 0
+   for event in pygame.event.get():
+       if event.type == pygame.QUIT:
+           running = False
 
-    for ball in balls:
-        ball.update()
-        ball.draw()
 
-    pygame.display.flip()
+   # spawn every 2 seconds
+   if spawn_timer > 2000:
+       side = random.choice(["left", "right"])
+
+
+       if side == "left":
+           balls.append([15, random.randint(50, 200), 6, -5])
+       else:
+           balls.append([585, random.randint(50, 200), -6, -5])
+
+
+       spawn_timer = 0
+
+
+   for ball in balls:
+       ball[3] += gravity
+       ball[0] += ball[2]
+       ball[1] += ball[3]
+
+
+       # bounce on floor
+       if ball[1] > 785:
+           ball[1] = 785
+           ball[3] *= -0.8
+
+
+       pygame.draw.circle(screen, (0, 200, 255),
+                          (int(ball[0]), int(ball[1])), 15)
+
+
+   pygame.display.flip()
+
 
 pygame.quit()
